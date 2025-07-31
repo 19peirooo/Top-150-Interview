478b8b8207f3f48d74b34e416e300685cfa32fa6
19peirooo <160737016+19peirooo@users.noreply.github.com>
2025-07-31 13:40:21 +0200

    Rotate Array Problem

--- Problems/rotate_array.py
+++ Problems/rotate_array.py
@@ -1,3 +1,6 @@
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+# Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+
 class Solution(object):
     def rotate1(self, nums, k):
         n = len(nums)
