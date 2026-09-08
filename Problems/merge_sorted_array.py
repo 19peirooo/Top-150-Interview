d53697745167dc9c1724de255d51a25d0713900a
19peirooo <160737016+19peirooo@users.noreply.github.com>
2026-09-08 10:54:01 +0200

    Cambios Rapidos

--- Problems/merge_sorted_array.py
+++ Problems/merge_sorted_array.py
@@ -10,8 +10,8 @@ class Solution:
        j = n - 1
        k = m + n - 1
 
-       while j >= 0 and k >= 0:
-            if nums1[i] >=  nums2[j]: 
+       while j >= 0:
+            if i >= 0 and nums1[i] >=  nums2[j]: 
                 nums1[k] = nums1[i]
                 i -= 1
             else: 
